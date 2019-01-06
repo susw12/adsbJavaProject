6334183f906830bc90b8062cd2d07480aa72cf59
Sujay Swain <sujaymswain@gmail.com>
2019-01-06 15:32:12 -0500

    More ****

--- interface.py
+++ interface.py
@@ -1,6 +1,9 @@
 import ml
 
-company = input("Please enter the name of the company: ")
 conference = input("Please enter the name of conference: ")
 
-ml.analysis("tweet.json", company, conference)
+file = open("name.txt",w)
+file.write(conference)
+file.close()
+
+print(ml.kowalski())
