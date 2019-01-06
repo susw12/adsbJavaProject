9ea58f9f747cc9e06b582310bf5e589ab2d5e21f
Sujay Swain <sujaymswain@gmail.com>
2019-01-06 15:28:18 -0500

    some more shit

--- interface.py
+++ interface.py
@@ -1,6 +1,6 @@
-import hashtags
+import ml
 
-company = input(print("Please enter the name of the company: "))
-conference = input(print("Please enter the name of conference: "))
+company = input("Please enter the name of the company: ")
+conference = input("Please enter the name of conference: ")
 
-hashtags.analysis("tweet.json", company, conference)
+ml.analysis("tweet.json", company, conference)
