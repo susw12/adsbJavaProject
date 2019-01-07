b682b4840c3585a3448a5552d9e9ce14811b90d2
jel20854 <jel20854@gmail.com>
2019-01-07 14:24:17 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,10 +1,8 @@
-import ml.py
+import ml
+import tweet_user_crawler
 
 conference = input("Please enter the name of conference: ")
 company = input("Please enter the name of conference: ")
 
-file = open("name.txt",w)
-file.write(conference)
-file.close()
-
-print(ml.flag())
+tweet_user_crawler.getTweets(company)
+print(ml.flag(conference))
