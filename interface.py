f01db4d619a36f6f593c4cb8d810a0bd166fbf0f
jel20854 <jel20854@gmail.com>
2019-01-07 14:16:27 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,9 +1,10 @@
-import ml
+import ml.py
 
 conference = input("Please enter the name of conference: ")
+company = input("Please enter the name of conference: ")
 
 file = open("name.txt",w)
 file.write(conference)
 file.close()
 
-print(ml.kowalski())
+print(ml.flag())
