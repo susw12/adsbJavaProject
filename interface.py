1836c5153d103a81f87f37632b718d85dd6726e8
jel20854 <jel20854@gmail.com>
2019-01-08 18:49:07 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -5,4 +5,4 @@ conference = input("Please enter the name of conference: ")
 company = input("Please enter the name of conference: ")
 
 tweet_user_crawler.getTweets(company)
-print(ml.flag(conference)) 
+ml.flag(conference, company) 
