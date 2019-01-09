ba5eb9d607356b969e77a392ecc2a00ca4a37f95
jel20854 <jel20854@gmail.com>
2019-01-08 19:00:37 -0500

    Update interface.py

--- interface.py
+++ interface.py
@@ -1,8 +1,10 @@
 import ml
 import tweet_user_crawler
 
-conference = input("Please enter the name of conference: ")
-company = input("Please enter the name of conference: ")
+company = input("Please enter the twitter handle of the company: ")
+conference = input("Please enter the twitter handle of the conference: ")
 
+print("Getting tweets from @" + company + " ...   ", end="")
 tweet_user_crawler.getTweets(company)
+print("Evaluating relationship between @" + company + "and @" + conference + " ...   ", end="")
 ml.flag(conference, company) 
