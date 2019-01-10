85102ce7f352be587a83a3143af3f1b0177e9b98
Sujay Swain <susw12@users.noreply.github.com>
2019-01-10 17:37:10 -0500

    Update ml.py

--- ml.py
+++ ml.py
@@ -13,74 +13,59 @@ def flag(conference, company):
     foundMentions = 0
     totalMentions = 0
     numTweets = 0
-    
-    f = open("tweets.json", "r")
-    for line in f:
-        hashtag = ""
-        hashtags = []
-        try:
-            hashtag = line[line.index("[",line.index("hashtags\":")) : line.index("]}]},",line.index("hashtags\":"))+1]
-            if hashtag[1] == "]":
-                hashtag = ""
-        except:
-            try:
-                hashtag = line[line.index("[",line.index("hashtags\":")) : line.index("],",line.index("hashtags\":"))+1]
-                if hashtag[1] == "]":
-                    hashtag = ""
-            except:
-                hashtag = ""	
-        if (len(hashtag) != 2 and len(hashtag) != 0):
-            hashtag = hashtag[1 : len(hashtag)-1].lower()
-            hashtags = hashtag.split("},")
-            for x in range(len(hashtags)):
-                try:
-                    hashtags[x] = (hashtags[x])[9 : hashtags[x].index("indices")-3]
-                except:
-                    hashtags[x] = ""
-    f.close()
 
+    lineNum = 0
     f = open("tweets.json", "r")
     for line in f:
-        mention = ""
-        mentions = []
-        try:
-            mention = line[line.index("[",line.index("user_mentions\":")) : line.index("]}]},",line.index("user_mentions\":"))+1]
-            if mention[1] == "]":
-                mention = ""
-        except:
-            try:
-                mention = line[line.index("[",line.index("user_mentions\":")) : line.index("}]",line.index("user_mentions\":"))+2]
-                if mention[1] == "]":
-                    mention = ""
-            except:
-                mention = ""
-        if (len(mention) != 2 and len(mention) != 0):
-            mention = mention[1 : len(mention)-1].lower()
-            mentions = mention.split("},")
-            for x in range(len(mentions)):
-                try:
-                    mentions[x] = (mentions[x])[mentions[x].index("screen_name\":")+14 : mentions[x].index("\",")]
-                except:
-                    mentions[x] = ""
+        if (lineNum % 2 == 0):
+            numTweets += 1
+            hashtag = line.strip()
+            hashtags = []
+            if (len(hashtag) == 2):
+                hashtag = ""
+            else:
+                hashtag = hashtag[1 : len(hashtag)-1].lower()
+                hashtags = hashtag.split("}, ")
+                for x in range(len(hashtags)):
+                    hashtags[x] = (hashtags[x])[10 : hashtags[x].index(", 'indices'")-1].strip()
+            for h in hashtags:
+                print(h + " : " + str(numTweets))
+                if h == conference:
+                    foundHashtags += 1
+        else:
+            mention = line.strip()
+            mentions = []
+            if (len(mention) == 2):
+                hashtag = ""
+            else:
+                mention = mention[1 : len(mention)-1].lower()
+                mentions = mention.split("}, ")
+                for x in range(len(mentions)):
+                    mentions[x] = (mentions[x])[17 : mentions[x].index(", 'name")-1].strip()
+            for m in mentions:
+                print(m + " : " + str(numTweets))
+                if m == conference:
+                    foundMentions += 1
+        lineNum += 1
+            
     f.close()
-        
-    for h in hashtags:
-        if h == conference:
-            foundHashtags += 1
-    for m in mentions:
-        if m == conference:
-            foundMentions += 1
 
     totalHashtags += len(hashtags)
     totalMentions += len(mentions)
-    numTweets += 1
-        
-    total = ((foundHashtags / numTweets) / averageHashtags + (foundMentions / numTweets) / averageMentions) / 2
+
+    print()
+    print("foundHashtags: " + str(foundHashtags))
+    print("foundMentions: " + str(foundMentions))
+    print("numTweets:" + str(numTweets))
+    print("averageHashtags: " + str(averageHashtags))
+    print("averageMentions: " + str(averageMentions))
+    total = ((float(foundHashtags) / float(numTweets)) / float(averageHashtags) + (float(foundMentions) / float(numTweets)) / float(averageMentions)) / 2
 
     toFlag = False
     found = False
     if total > 0.5:
         toFlag = True
+        """
         lines = open("dictionary.txt", "r").readlines()
         for lineNum in range(len(lines)):
             line = f.readline()
@@ -97,23 +82,25 @@ def flag(conference, company):
             f = open("dictionary.txt", "a")
             f.write(conference + ":[[" + foundHashtags + "," + foundMentions + "]]")
             f.close()
+        """
             
     averageHashtags = ((averageHashtags*numTweetsH)+totalHashtags) / (numTweetsH + numTweets)
     f = open("averageHashtags.txt", "w")
-    f.write(averageHashtags)
-    f.write(numTweetsH+numTweets)
+    f.write(str(averageHashtags))
+    f.write("\n")
+    f.write(str(numTweetsH+numTweets))
     f.close()
     averageMentions = ((averageMentions*numTweetsM)+totalMentions) / (numTweetsM + numTweets)
     f = open("averageMentions.txt", "w")
-    f.write(averageMentions)
-    f.write(numTweetsM+numTweets)
+    f.write(str(averageMentions))
+    f.write("\n")
+    f.write(str(numTweetsM+numTweets))
     f.close()
 
     print("Done.\n")
     
-    print("In "+"numTweets" + " tweets, " + "twitter user @" + company + " used " + totalHashtags + " hashtags and " + totalMentions + "mentions of the twitter user @" + conference)
+    print("In "+ str(numTweets) + " tweets, " + "twitter user @" + company + " used " + str(foundHashtags) + " hashtags and " + str(foundMentions) + " mentions of the twitter user @" + conference)
     if toFlag:
-        print("@" + company + " and @" + conference + " were found to have a " + total + " correlation with each other, indicating the presence of a strong relationship.")
+        print("@" + company + " and @" + conference + " were found to have a " + str(total) + " correlation with each other, indicating the presence of a strong relationship.")
     else:
-        print("@" + company + " and @" + conference + " were found to have a " + total + " correlation with each other, indicating the lack of a strong relationship.")
-
+        print("@" + company + " and @" + conference + " were found to have a " + str(total) + " correlation with each other, indicating the lack of a strong relationship.")
