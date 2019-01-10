8dfb97d9fcc6348ea81f30e8bc27a2b1548e7047
Sujay Swain <sujaymswain@gmail.com>
2019-01-09 23:07:44 -0500

    Java program works

--- interface.py
+++ interface.py
@@ -2,10 +2,14 @@ import ml
 import tweet_user_crawler
 import sys
 
-company = sys.argv[1]
-conference = sys.argv[2]
+hello = sys.argv
+
+company = hello[1]
+conference = hello[2]
 
 print("Getting tweets from @" + company + " ...   ", end="")
 tweet_user_crawler.getTweets(company)
 print("Evaluating relationship between @" + company + " and @" + conference + " ...   ", end="")
 ml.flag(conference, company)  
+
+
--- test.java
+++ test.java
@@ -1,18 +1,26 @@
 import java.util.Scanner;
-import java.io.IOException;
+import java.io.*;
 
 public class test{ 
-    public static void main(String[] args) throws IOException{
+    
+    public static void main(String args[]) throws IOException{
+        String s = null;        
         Scanner userInput = new Scanner(System.in);
         System.out.print("Please enter the twitter handle of the company: ");
         String company = userInput.nextLine();
         System.out.print("\nPlease enter the twitter handle of the conference: ");
         String conference = userInput.nextLine();
         try {
-            Process p = Runtime.getRuntime().exec("python interface.py company conference");
-            System.out.println(p);
+            Process p = Runtime.getRuntime().exec("python3 interface.py " + company + " " + conference);
+            BufferedReader stdInput = new BufferedReader(new InputStreamReader(p.getInputStream()));
+
+            while ((s = stdInput.readLine()) != null) {
+                System.out.println(s);
+            }
+            
         } catch (IOException e1) {
             System.out.println("Shoot!");
         }
+        userInput.close();
     }
 }
